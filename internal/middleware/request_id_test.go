bb68542ff2f65547b1564312dbd345b26d809e35
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-07-24 19:12:01 +0200

    Tests for Request id middleware added

---
+++ internal/middleware/request_id_test.go
@@ -0,0 +1,62 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestIfRequestIdIsPresentItIsExtended(t *testing.T) {
+	// GIVEN
+	requestId := "foo:bar"
+	ctx := gin.Context{
+		Request: &http.Request{ Header: make(http.Header)},
+	}
+	ctx.Request.Header.Set(requestIdHeaderName, requestId)
+	middleware := RequestId()
+
+	// WHEN
+	middleware(&ctx)
+
+	// THEN
+	val := ctx.Request.Header.Get(requestIdHeaderName)
+	require.NotEmpty(t, val, "X-Request-Id header must not be empty")
+
+	reqIdComponents := strings.Split(val, ";")
+	require.Equal(t, len(reqIdComponents), 2,"X-Request-Id must consist of two components")
+	require.Equal(t, reqIdComponents[0], requestId, "First X-Request-Id component must be equal the value of the sent X-Request-Id")
+
+	setReqId := strings.Split(reqIdComponents[1], ":")
+	require.Equal(t, len(setReqId), 2,"Set X-Request-Id component must consist of two parts")
+	require.Equal(t, setReqId[0], "login-provider", "First part of the set X-Request-Id component must be 'login-provider'")
+	require.NotEmpty(t, setReqId[1], "Second part of the set X-Request-Id component must not be empty")
+}
+
+func TestIfRequestIdIsNotPresentItIsAdded(t *testing.T) {
+	// GIVEN
+	ctx := gin.Context{
+		Request: &http.Request{ Header: make(http.Header)},
+	}
+	middleware := RequestId()
+
+	// WHEN
+	middleware(&ctx)
+
+	// THEN
+	val := ctx.Request.Header.Get(requestIdHeaderName)
+	require.NotEmpty(t, val, "X-Request-Id header must not be empty")
+
+	reqIdComponents := strings.Split(val, ";")
+	require.Equal(t, len(reqIdComponents), 1,"X-Request-Id must consist of one component")
+
+	setReqId := strings.Split(reqIdComponents[0], ":")
+	require.Equal(t, len(setReqId), 2,"Set X-Request-Id component must consist of two parts")
+	require.Equal(t, setReqId[0], "login-provider", "First part of the set X-Request-Id component must be 'login-provider'")
+	require.NotEmpty(t, setReqId[1], "Second part of the set X-Request-Id component must not be empty")
+}
+
+func TestRequestIdMiddlewareCallsContextNext(t *testing.T) {
+	// TODO: How to achieve that. gin.Context is a struct and as such not mockable
+}
