ec426318fbd95dc33845287dd1d88a98f0478542
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-07-24 18:00:36 +0200

    tests for correlation id middleware added

---
+++ internal/middleware/correlation_id_test.go
@@ -0,0 +1,62 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+type MockedResponseWriter struct {
+	gin.ResponseWriter
+	header http.Header
+}
+
+func (rw *MockedResponseWriter) Header() http.Header {
+	if rw.header == nil {
+		rw.header = make(http.Header)
+	}
+
+	return rw.header
+}
+
+func TestIfCorrelationIdIsPresentItIsReused(t *testing.T) {
+	// GIVEN
+	correlationId := "foo"
+	ctx := gin.Context{
+		Request: &http.Request{ Header: make(http.Header)},
+		Writer:  &MockedResponseWriter{},
+	}
+	ctx.Request.Header.Set(headerName, correlationId)
+	middleware := CorrelationId()
+
+	// WHEN
+	middleware(&ctx)
+
+	// THEN
+	val := ctx.Writer.Header().Get(headerName)
+	require.NotEmpty(t, val, "Correlation-Id header must not be empty")
+	require.Equal(t, correlationId, val, "Set and received correlation ids must be equal")
+}
+
+func TestIfCorrelationIdIsNotPresentItIsCreated(t *testing.T) {
+	// GIVEN
+	ctx := gin.Context{
+		Request: &http.Request{ Header: make(http.Header)},
+		Writer:  &MockedResponseWriter{},
+	}
+	middleware := CorrelationId()
+
+	// WHEN
+	middleware(&ctx)
+
+	// THEN
+	reqVal := ctx.Writer.Header().Get(headerName)
+	respVal := ctx.Request.Header.Get(headerName)
+	require.NotEmpty(t, reqVal, "Correlation-Id header must not be empty")
+	require.Equal(t, reqVal, respVal, "Request and response correlation ids must be equal")
+}
+
+func TestMiddlewareCallsContextNext(t *testing.T) {
+	// TODO: How to achieve that. gin.Context is a struct and as such not mockable
+}
