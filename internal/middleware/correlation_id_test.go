807e85272d3d003f3f6528bdab90747c9d5edb3d
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-07-24 19:16:58 +0200

    moved reusable test code to separate file

--- internal/middleware/correlation_id_test.go
+++ internal/middleware/correlation_id_test.go
@@ -7,19 +7,6 @@ import (
 	"testing"
 )
 
-type MockedResponseWriter struct {
-	gin.ResponseWriter
-	header http.Header
-}
-
-func (rw *MockedResponseWriter) Header() http.Header {
-	if rw.header == nil {
-		rw.header = make(http.Header)
-	}
-
-	return rw.header
-}
-
 func TestIfCorrelationIdIsPresentItIsReused(t *testing.T) {
 	// GIVEN
 	correlationId := "foo"
